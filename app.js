f6e37c892b428b9265b8ce313da41bfbab2f0101
Szymon Zygaluk <szymon.zygaluk@gmail.com>
2020-07-24 15:59:33 +0200

    Basic scripts and some styling added.

--- app.js
+++ app.js
@@ -1,6 +1,34 @@
 new Vue ({
   el: "#app",
   data: {
-    text: "Test"
+    text: "Test",
+    clientName: '',
+    confirmed: '',
+    briefType: '',
+    setStatus: '',
+    whoFor: '',
+    whoWith: '',
+    startDate: '',
+    meetDate: '',
+    deadline: '',
+    delDate: '',
+    tableContents: []
+  },
+  methods: {
+    addRow() {
+      this.tableContents.push({
+        clientName: this.clientName,
+        confirmed: this.confirmed,
+        briefType: this.briefType,
+        setStatus: this.setStatus,
+        whoFor: this.whoFor,
+        whoWith: this.whoWith,
+        startDate: this.startDate,
+        meetDate: this.meetDate,
+        deadline: this.deadline,
+        delDate: this.delDate
+      })
+      this.clientName = ""
+    }
   }
 })
