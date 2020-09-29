8d34dd9a1947bff8974fd7ea51646fbf66703e2c
Szymon <szulanowski@gmail.com>
2020-09-29 21:27:09 +0200

    Setting up the searchbox for dynamic search (as it used to work)

--- src/components/BoxGrids/BoxesAll.vue
+++ src/components/BoxGrids/BoxesAll.vue
@@ -1,9 +1,103 @@
 <template>
-  <div :class="{'redBod': redBackground}">
-      <app-navbar></app-navbar>
-      <app-box 
-      :boxes="filteredList" 
-      @boxRemoved="removeBox"></app-box>
+  <div :class="{ redBod: redBackground }">
+    <div style="padding-top: 100px">
+      <nav
+        style="height: 100px"
+        class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark"
+      >
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav col-1">
+            <li class="nav-item">
+              <button class="btn btn-success" @click="addBoxDisplay">
+                Add New Box
+              </button>
+            </li>
+          </ul>
+          <form class="form-inline mx-auto my-lg-0 col">
+            <ul class="nav nav-pills mr-2">
+              <li class="nav-item" @click="changeLink('In Progress')">
+                <a
+                  class="nav-link"
+                  style="color: white"
+                  active-class="active"
+                  exact
+                  >In Progress</a
+                >
+              </li>
+              <li class="nav-item" @click="changeLink('Pending')">
+                <a
+                  :to="{ name: 'pending' }"
+                  class="nav-link"
+                  style="color: white"
+                  active-class="active"
+                  exact
+                  >Pending</a
+                >
+              </li>
+              <li class="nav-item" @click="changeLink('Done')">
+                <a
+                  :to="{ name: 'done' }"
+                  class="nav-link"
+                  style="color: white"
+                  active-class="active"
+                  exact
+                  >Done</a
+                >
+              </li>
+
+              <li class="nav-item" @click="changeLink('All')">
+                <a
+                  :to="{ name: 'all' }"
+                  class="nav-link"
+                  style="color: white"
+                  active-class="active"
+                  exact
+                  >All</a
+                >
+              </li>
+            </ul>
+            <select class="form-control" v-model="searchForBox">
+              <option value selected disabled hidden>
+                Choose a client from a dropdown list:
+              </option>
+              <option v-for="each in filteredList">{{ each.clientName }}</option>
+            </select>
+            <p style="margin: 10px; color: white">or</p>
+            <input
+              class="form-control mr-sm-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+              v-model="searchForBox"
+            />
+            <p>{{ searchForBox }}</p>
+            <button
+              class="btn btn-outline-danger my-2 my-sm-0"
+              type="submit"
+              @click.prevent="refreshInput"
+            >
+              Clear
+            </button>
+          </form>
+          <button class="btn btn-danger">Log out</button>
+        </div>
+      </nav>
+      <div>
+        <ul class="nav nav-pills">
+          <a to="/" tag="li" active-class="active" exact>
+            <a class="nav-link">All</a>
+          </a>
+          <a to="/wip" tag="li" active-class="active">
+            <a class="nav-link">WIP</a>
+          </a>
+          <li class="nav-link" style="color: red">
+            THIS WILL GET DELETED LATER
+          </li>
+        </ul>
+      </div>
+    </div>
+    <!--<app-navbar></app-navbar>-->
+    <app-box :boxes="filteredList" @boxRemoved="removeBox"></app-box>
   </div>
 </template>
 
@@ -11,7 +105,7 @@
 import { mapGetters } from "vuex";
 import { tableBus } from "../../main.js";
 import Box from "../Boxes/Box.vue";
-import Navbar from "../Navbar.vue"
+import Navbar from "../Navbar.vue";
 export default {
   data() {
     return {
@@ -22,17 +116,17 @@ export default {
     };
   },
   computed: {
-    ...mapGetters([
-      'allBoxContents',
-      'filteredList'
-    ]),
+    ...mapGetters(["allBoxContents", "filteredList"]),
   },
   components: {
     appBox: Box,
-    appNavbar: Navbar
+    appNavbar: Navbar,
   },
   methods: {
-        changeLink(value) {
+    addBoxDisplay() {
+      this.$store.dispatch("addBoxDisplay");
+    },
+    changeLink(value) {
       this.$store.commit("changeLink", value);
     },
     removeBox(idNumber) {
